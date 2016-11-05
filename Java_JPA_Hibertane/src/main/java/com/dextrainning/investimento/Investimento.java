703d672529397ece1cd4c8f40456be26a79ce32b
Everton Peres <evertonluperes@gmail.com>
2016-11-05 17:40:05 -0200

    JPA_05_11-2016

---
+++ Java_JPA_Hibertane/src/main/java/com/dextrainning/investimento/Investimento.java
@@ -0,0 +1,72 @@
+package com.dextrainning.investimento;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Investimento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private Double valor;
+	
+	@Column(nullable = false)
+	private String descricao;
+
+	@Column(nullable = false)
+	private Double rendimentoMensal;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="data_hora")
+	private Date data;
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getRendimentoMensal() {
+		return rendimentoMensal;
+	}
+
+	public void setRendimentoMensal(Double rendimentoMensal) {
+		this.rendimentoMensal = rendimentoMensal;
+	}
+}
